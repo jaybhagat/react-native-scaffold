50cfaec0cf58b77f1a98d59280927d42e0ec197c
yathinosaur <yathinosaur@gmail.com>
2020-08-22 23:05:59 -0400

    added gitignore and filesharing component

---
+++ src/components/FileSharing/FileSharing.js
@@ -0,0 +1,16 @@
+const { Button } = require("react-native");
+
+import React from 'react';
+
+class FireSharing extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+
+
+    render(){
+        return  <Button>Share Info</Button>
+    }
+}
